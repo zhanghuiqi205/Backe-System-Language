4c1115b04f925ff70241e7730ef0e8b9e4de7268
张惠奇 <649676852@qq.com>
2017-12-18 16:00:40 +0800

    增加文件

---
+++ PHP/common.php
@@ -0,0 +1,192 @@
+<?php
+
+/**
+ * 绘制验证码
+ */
+$w = 100;
+$h = 30;
+$len = 4;
+captcha($w,$h,$len);
+function captcha($w,$h,$len){
+	// 1、创建画布
+	$img = imagecreatetruecolor($w,$h);
+
+	// 2、填充背景颜色
+	$bg = imagecolorallocate($img,mt_rand(210,255),mt_rand(210,255),mt_rand(210,255));
+	imagefill($img,0,0,$bg);
+
+	// 3、绘制随机验证码
+	$code = getCode($len);
+	for($i=0;$i<strlen($code);$i++){
+		$color = imagecolorallocate($img,mt_rand(110,200),mt_rand(110,200),mt_rand(110,200));
+		imagettftext($img,mt_rand(18,26),mt_rand(-20,20),18*($i+1),25,$color,'simhei.ttf',$code[$i]);
+	}
+
+	// 4、增加干扰线
+	for($i=0;$i<4;$i++){
+		$color = imagecolorallocate($img,mt_rand(110,200),mt_rand(110,200),mt_rand(110,200));
+		imageline($img,mt_rand(0,100),mt_rand(0,30),mt_rand(0,100),mt_rand(0,30),$color);
+	}
+
+	// 测试
+	header('content-type:image/jpeg');
+	imagejpeg($img);
+}
+
+//生成随机的字符串
+function getCode($len){
+	$charset = 'abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ23456789';		//57
+	$code = '';
+	for($i=0;$i<$len;$i++){
+		$code .= $charset[mt_rand(0,56)];
+	}
+	return $code;
+}
+
+/**
+ * 缩略图
+ */
+$d_w = 400;
+$d_h = 200;
+$src = "game.jpg";
+thumb($d_w,$d_h,$src);
+function thumb($d_w,$d_h,$src){
+	//创建画布(框)
+	$imgDest = imagecreatetruecolor($d_w,$d_h);		//框
+	$bg = imagecolorallocate($imgDest,250,250,250);
+	imagefill($imgDest,0,0,$bg);
+
+	//由用户图片创建画布
+	$infoSrc = getimagesize($src);
+	$s_w = $infoSrc[0];
+	$s_h = $infoSrc[1];
+	$imgSrc = createFrom($src);
+	$f_h = $d_h;
+	$f_w = $s_w/$s_h*$f_h;
+	if($f_w>$d_w){
+		$f_w=$d_w;
+		$f_h=$f_w*$s_h/$s_w;
+	}
+	//计算imgDest上所放置的起点位置
+	$posX = ($d_w-$f_w)/2;
+	$posY = ($d_h-$f_h)/2;
+	//采样合并
+	imagecopyresampled($imgDest,$imgSrc,$posX,$posY,0,0,$f_w,$f_h,$s_w,$s_h);
+	header('content-type:image/jpeg');
+	imagejpeg($imgDest);
+}
+
+//封装由图片创建画布的函数
+function createFrom($file){
+
+	$info = getimagesize($file);
+
+	switch($info['mime']){
+		case 'image/jpeg':
+			$img = imagecreatefromjpeg($file);
+			break;
+		case 'image/png':
+			$img = imagecreatefrompng($file);
+			break;
+		case 'image/gif':
+			$img = imagecreatefromgif($file);
+			break;
+	}
+
+	return $img;
+}
+
+/**
+ * 水印的制作
+*@param1 dest string 用户图片
+*@param2 src string 公司logo
+*@param3 int 表示logo的位置  1:左上角，2：右上角，3：左下角，4：右下角，5：中间
+*@param4 op int 透明度
+*/
+//用户上传的图片
+$dest = 'game.jpg';
+//公司的logo
+$src = 'logo.png';
+$op = 50;
+$pos = 4;
+water($dest,$src,$pos,$op);
+
+
+function water($dest,$src,$pos=2,$op=60){
+   
+   $infoDest = getimagesize($dest);
+   $infoSrc = getimagesize($src);
+
+   //将已有的图片读取到画布中
+   $imgDest = createFrom($dest);
+   $imgSrc = createFrom($src);
+
+   switch($pos){
+       case 1:
+           $d_x = 0;
+           $d_y = 0;
+           break;
+       case 2:
+           $d_x = $infoDest[0]-$infoSrc[0];
+           $d_y = 0;
+           break;
+       case 3:
+           $d_x = 0;
+           $d_y = $infoDest[1]-$infoSrc[1];
+           break;
+       case 4:
+           $d_x = $infoDest[0]-$infoSrc[0];
+           $d_y = $infoDest[1]-$infoSrc[1];
+           break;
+       case 5:
+           $d_x = ($infoDest[0]-$infoSrc[0])/2;
+           $d_y = ($infoDest[1]-$infoSrc[1])/2;
+           break;
+   }
+
+   //制作水印
+   imagecopymerge($imgDest,$imgSrc,$d_x,$d_y,0,0,$infoSrc[0],$infoSrc[1],$op);
+
+   header('Content-type:image/jpeg');
+   imagejpeg($imgDest);
+}
+
+
+/**
+ *遍历文件夹
+ * 
+ */
+$dir = "app";
+function getTree($dir){
+	$arr = scandir($dir);
+	foreach($arr as $v){
+		$item = $dir.'/'.$v;
+		//判断是不否是文件夹
+		if(is_dir($item)){
+			//如果是文件夹，且不是. 或.. 才继续遍历
+			if($v == '.' || $v=='..'){
+				continue;
+			}
+			echo '<font color="red">'.$item.'</font>','<br/>';
+			getTree($item);
+
+		}else{
+			//如果不是就直接输出
+			echo '<font color="blue">'.$item.'</font>','<br/>';
+		}
+		
+	}
+}
+getTree($dir);
+
+
+
+
+
+
+
+
+
+
+
+
